7371b82c71be4537dc8e184adfc729ea1b5987ef
Aleksander <aleksander_pettherson@hotmail.com>
2019-03-19 14:23:37 -0300

    Update name Class at migrations

--- database/migrations/2019_03_12_232938_create_contact_suppliers.php
+++ database/migrations/2019_03_12_232938_create_contact_suppliers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContactSupplies extends Migration
+class CreateContactSuppliers extends Migration
 {
     /**
      * Run the migrations.
