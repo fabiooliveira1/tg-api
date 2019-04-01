df3a5fa84067b2a774aac4ab47dd85756a984595
Aleksander <aleksander_pettherson@hotmail.com>
2019-04-01 18:19:31 -0300

    Created model User and BillsGroup

---
+++ app/Models/BillsGroup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Api\Models;
+
+
+class BillsGroup extends BaseModel
+{
+    
+    public $fillable = [
+        'GrCt_idRisco',
+        'GrCt_NomeGrupo',
+        'GrCt_DescrGrupo'
+    ];
+
+    public $dates = ['created_at', 'updated_at'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        
+    }
+
+    public function bill()
+    {
+        return $this->belongsTo(Bill::class);
+    }
+
+    public function risk()
+    {
+        return $this->hasOne(Risk::class);
+    }
+
+    public function requereds()
+    {
+        return $this->hasMany(Requered::class);
+    }
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Api\Models;
+
+
+class User extends BaseModel
+{
+    
+    public $fillable = [
+        'User_nivelAcesso',
+        'User_matricula',
+        'User_senha',
+        'User_nome',
+        'User_email'
+    ];
+
+    public $dates = ['created_at', 'updated_at'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+    }
+
+    public function bills()
+    {
+        return $this->hasMany(Bill::class);
+    }
+
+
+}
