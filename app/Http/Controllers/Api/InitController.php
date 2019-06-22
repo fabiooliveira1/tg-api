a31fbbd97f7021df067218b7b176621480266012
fabiooliveira1 <fabioaurelio1269@gmail.com>
2019-06-22 14:05:07 -0300

    Add Dashboard

--- app/Http/Controllers/Api/InitController.php
+++ app/Http/Controllers/Api/InitController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use Carbon\Carbon;
+use App\Repositories\BillsRepository;
+use App\Repositories\RenegotiationsRepository;
+use App\Repositories\SimulationsRepository;
 use App\Repositories\BanksRepository;
 use App\Repositories\PaymentWaysRepository;
 use App\Repositories\RequiredsRepository;
@@ -11,6 +15,58 @@ use App;
 
 class InitController extends Controller
 {
+  public function index()
+  {
+    return [
+      'requireds' => $this->getRequiredsRepository()->getModel()->all(),
+      'risks' => $this->getRisksRepository()->getModel()->all(),
+      'banks' => $this->getBanksRepository()->getModel()->all(),
+      'paymentWays' => $this->getPaymentWaysRepository()->getModel()->all(),
+    ];
+  }
+
+  public function dashboard()
+  {
+    return [
+      'openBills'         => $this->getBillsRepository()->getModel()
+                              ->where('Cta_Status', 'Aberta')
+                              ->whereDate('Cta_dataVencimento', '>=', Carbon::now())
+                              ->count(),
+      'overdueBills'      => $this->getBillsRepository()->getModel()
+                              ->where('Cta_Status', 'Aberta')
+                              ->whereDate('Cta_dataVencimento', '<', Carbon::now())
+                              ->count(),
+      'renegociatedBills' => $this->getBillsRepository()->getModel()
+                              ->where('Cta_Status', 'Renegociada')
+                              ->count(),
+      'openSimulations'   => $this->getSimulationsRepository()->getModel()
+                              ->where('Sim_status', 'Pendente')
+                              ->count(),
+      'closeToDueDate'    => $this->getBillsRepository()->getModel()
+                              ->where('Cta_Status', 'Aberta')
+                              ->whereDate('Cta_dataVencimento', '>=', Carbon::now())
+                              ->orderBy('Cta_dataVencimento', 'asc')
+                              ->limit(5)
+                              ->with(['supplier', 'billsGroup'])
+                              ->get()
+    ];
+  }
+
+  public function getBillsRepository()
+  {
+    return app(BillsRepository::class);
+  }
+
+  public function getRenegotiationsRepository()
+  {
+    return app(RenegotiationsRepository::class);
+  }
+
+  public function getSimulationsRepository()
+  {
+    return app(SimulationsRepository::class);
+  }
+
   public function getBanksRepository()
   {
     return app(BanksRepository::class);
@@ -29,15 +85,5 @@ class InitController extends Controller
   public function getRisksRepository()
   {
     return app(RisksRepository::class);
-  }
-
-  public function index()
-  {
-    return [
-      'requireds' => $this->getRequiredsRepository()->getModel()->all(),
-      'risks' => $this->getRisksRepository()->getModel()->all(),
-      'banks' => $this->getBanksRepository()->getModel()->all(),
-      'paymentWays' => $this->getPaymentWaysRepository()->getModel()->all(),
-    ];
   }
 }
